a1fd7e440677146daabc7a5ed0f6e1f41d75e6a2
ivanpagac <ivanpagac@gmail.com>
2020-02-29 13:14:01 +0100

    Add files via upload

---
+++ remove_rctwebview.sh
@@ -0,0 +1,11 @@
+function remove_rctwebview(){
+    
+    local dir="${BASE_DIR}/node_modules/react-native/React";
+
+    sed -i'.bak' '/RCTWebView/d' "${dir}/React.xcodeproj/project.pbxproj"
+    rm -f "${dir}/React.xcodeproj/project.pbxproj.bak"
+    rm -f "${dir}/Views/RCTWebView.m"
+    rm -f "${dir}/Views/RCTWebView.h"
+    rm -f "${dir}/Views/RCTWebViewManager.m"
+    rm -f "${dir}/Views/RCTWebViewManager.h"
+}
